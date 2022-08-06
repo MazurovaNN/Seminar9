7a2b3b6e83f7d581e45621e9c4722444b48bb2de
natali <anigama@mail.ru>
2022-08-06 20:23:22 +0300

    задача4

--- Desktop/GB/Program.cs
+++ Desktop/GB/Program.cs
@@ -1,13 +1,18 @@
-﻿Console.WriteLine("Введите первое число: ");
+﻿Console.WriteLine("Введите 3 числа:");
 
-int a = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-int b = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
 
-if (a>b)
-Console.WriteLine(a);
-else
-Console.WriteLine(b);    
-    
+int max = num1;
+if (num2 > max)
+{
+max = num2;
+}
+if (num3 > max)
+{
+max = num3;
+}
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);

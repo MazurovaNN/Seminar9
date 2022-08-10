59815cd010d69bfe5e8aa27d3ff9de10457803e9
natali <anigama@mail.ru>
2022-08-10 23:13:08 +0300

    решение задачи с опечаткой при поиске числа слева направо

---
+++ Documents/Цифровые профессии/С#/с2z13/Program1.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+ или сообщает, что третьей цифры нет. 
+ (Строки и циклы в решении использовать нельзя!)
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Введи число: ");
+
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+
+string anyNumberText = Convert.ToString(anyNumber);
+
+if (anyNumberText.Length > 2){
+
+Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+
+}
+
+else {
+
+Console.WriteLine("-> третьей цифры нет");
+
+}
+
+
+

887108b4dc162f6f6ec5cebedf3d14359bd30564
natali <anigama@mail.ru>
2022-08-20 13:02:59 +0300

    решение задачи со звездочкой2

---
+++ Documents/Цифровые профессии/С#/Семинар5/Seminar5/ProgramStar2.cs
@@ -0,0 +1,41 @@
+﻿/*Задача "со звездочкой": Разобраться с алгоритмом сортировки методом пузырька.
+ Реализовать невозрастающую сторировку. 
+[3, 0, 2, 4, -1] -> [4, 3, 2, 0, -1]
+[1,2,2,3,2] -> [3, 2, 2, 2, 1]
+*/ 
+
+static int[] BubbleSort(int[] mas)
+{
+    int temp;
+	for (int i = 0; i < mas.Length; i++)
+	{
+	for (int j = i + 1; j < mas.Length; j++)
+	{
+	if (mas[i] < mas[j])
+	{
+	temp = mas[i];
+	mas[i] = mas[j];
+    mas[j] = temp;
+	}                   
+	}            
+	}
+	return mas;
+}
+
+{
+    Console.WriteLine("Сколько чисел будем сортировать?");
+    int N = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите числа для сортировки:");
+    int[] mas=new int[N];
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    BubbleSort(mas);
+    Console.WriteLine("После сортировки:");
+    for (int i = 0; i < mas.Length; i++)
+    {
+        Console.WriteLine(mas[i]);
+    }
+    Console.ReadLine();
+}

87433139d5a056ded4f3eb9e354d3b4e823b47d6
natali <anigama@mail.ru>
2022-08-24 22:47:57 +0300

    решение задачиStar3

---
+++ Documents/Цифровые профессии/С#/Семинар6/Se6/Program.cs
@@ -0,0 +1,12 @@
+int main (){ 
+      int arr [2][4] = {{1,2,4},{3,4}}; 
+      for (int i=0; i<2; i++){ // 1 цикл 
+            for (int j=0; j<4; j++) // 2 цикл 
+                  printf("%d\t",arr[i][j]); 
+            printf("\n"); 
+      } 
+return 0; 
+}  
+
+
+
